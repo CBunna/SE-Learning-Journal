19ddfc23847d6ff9d440d3876878f1cd073971a7
CBunna <153239183+CBunna@users.noreply.github.com>
2026-03-05 15:08:03 +0100

    init: create React project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>My Learning Journal</h1>
+      <p>Track what you learn every day.</p>
+    </>
+  )
+}
+
+export default App
